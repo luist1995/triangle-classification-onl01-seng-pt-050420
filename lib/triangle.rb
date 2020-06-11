664a580fa4efc3f91ce985923fdfeb7f628e2bc0
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-06-11 00:33:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
   
   def valid?
     side_1 <= 0 || side_2 <= 0 || side_3 <= 0 || side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1
-    raise TriangleError
+    #raise TriangleError
   end
 end
 
